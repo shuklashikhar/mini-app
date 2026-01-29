2f38f203f1a3f316be1912a690b95b67b674b3dc
shuklashikhar <shuklashikhar2004@gmail.com>
2026-01-29 11:06:55 +0530

    add login api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import cors from "cors";
 import { query } from "./db/index.js";
+
+const JWT_SECRET = "secretkey";
+
 
 const app = express();
 
@@ -14,6 +19,44 @@ app.get("/health", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ status: "error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+   if (!email || !password) {
+    return res.status(400).json({ message: "Missing email or password" });
+  }
+
+  try {
+    const result = await query(
+      "SELECT id, email, password_hash FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const user = result.rows[0];
+
+    const isValid = await bcrypt.compare(password, user.password_hash);
+
+    if (!isValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 });
 

d9f05db800314189de09415614133a6127428a86
shuklashikhar <shuklashikhar2004@gmail.com>
2026-02-01 15:10:02 +0530

    resolve CORS issue for Render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+   "https://mini-app-1-8hgn.onrender.com"],
   credentials: true
 }));
 
